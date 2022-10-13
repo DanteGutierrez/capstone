8d60bcb4c3d1a313757bcfeedc212b17962414f3
DanteGutierrez <78101352+DanteGutierrez@users.noreply.github.com>
2022-10-13 14:23:39 -0600

    setup

---
+++ backend/course/Course.cs
@@ -0,0 +1,16 @@
+namespace capstone {
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization.Attributes;
+    public class Course {
+        [BsonId]
+        public ObjectId _id { get; set; }
+        [BsonElement("Name")]
+        public string? Name { get; set; }
+        [BsonElement("Code")]
+        public string? Code { get; set; }
+        public Course()
+        {
+            _id = ObjectId.GenerateNewId();
+        }
+    }
+}
--- backend/course/Program.cs
+++ backend/course/Program.cs
@@ -1,5 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Steeltoe.Discovery.Client;
+using Steeltoe.Common.Discovery;
+using Steeltoe.Discovery.Eureka;
+using Steeltoe.Discovery;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDiscoveryClient(builder.Configuration);
+var Origins = "_OriginsAllowed";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: Origins,
+                      policy =>
+                      {
+                          policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                      });
+});
+
 var app = builder.Build();
+
+app.MapControllers();
+app.UseCors(Origins);
 
 app.MapGet("/", () => "Hello World!");
 
